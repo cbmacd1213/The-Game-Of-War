51e2e1aab4309abe12427b5554a047220bcecea0
Colin's MacBook <colinsmacbook@Colinss-MacBook-Air.local>
2020-08-10 16:25:03 -0400

    update

--- apps.js
+++ apps.js
@@ -1,8 +1,18 @@
 console.log("HelloWorld")
 
 //-----------Constants--------------//
+// playerOne
+// computer
+
+const deck = [
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'],
+values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+];
+console.log(deck[3]);
 
 
+//drawDeck
+//reserveDeck
 //--------State Variables-----------//
 
 
@@ -15,7 +25,9 @@ console.log("HelloWorld")
 
 //---------Event Listeners----------//
 
-
+//onClickOfDrawDeck
+//ifMaxCardsisSucceeded
+//ifMinCardsisBested
 
 
 

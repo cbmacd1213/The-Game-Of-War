b2de7bffbc631255820344b23d38f0e2ee2b84fe
Colin's MacBook <colinsmacbook@Colinss-MacBook-Air.local>
2020-08-13 10:04:05 -0400

    css fix

--- apps.js
+++ apps.js
@@ -51,9 +51,29 @@ function init() {
     compCards = secondHalf;
 }
 
-function render(){
+function renderShuffledDeck() {
+    // Create a copy of the masterDeck (leave masterDeck untouched!)
+    const tempDeck = [...masterDeck];
+    shuffledDeck = [];
+    while (tempDeck.length) {
+      // Get a random index for a card still in the tempDeck
+      const rndIdx = Math.floor(Math.random() * tempDeck.length);
+      // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+      shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
+    renderDeckInContainer(shuffledDeck, shuffledContainer);
+  }
 
-}
+
+  function renderDeckInContainer(deck, container) {
+    container.innerHTML = '';
+    // Let's build the cards as a string of HTML
+    // Use reduce when you want to 'reduce' the array into a single thing - in this case a string of HTML markup 
+    const cardsHtml = deck.reduce(function(html, card) {
+      return html + `<div class="card ${card.face}"></div>`;
+    }, '');
+    container.innerHTML = cardsHtml;
+  }
 
 function shuffleDeck(deck) {
     const tempDeck = [...deck];
